19f5d41afc74e195c2f3c3935d6248fd61455b33
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2017-01-11 11:20:01 +0100

    Configure umbrella header and point to with modulemap

---
+++ Classes/JAMSVGImage/Supporting Files/JAMSVGImage-Umbrella.h
@@ -0,0 +1,25 @@
+//
+//  JAMSVGImage-Umbrella.h
+//  JAMSVGImage
+//
+//  Created by Cihat Gündüz on 11.01.17.
+//  Copyright © 2017 Jeff Menter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JAMSVGImage iOS.
+FOUNDATION_EXPORT double JAMSVGImage_VersionNumber;
+
+//! Project version string for JAMSVGImage iOS.
+FOUNDATION_EXPORT const unsigned char JAMSVGImage_VersionString[];
+
+#import <JAMSVGImage/JAMSVGImage.h>
+#import <JAMSVGImage/UIImage+SVG.h>
+#import <JAMSVGImage/JAMSVGImageView.h>
+#import <JAMSVGImage/JAMSVGButton.h>
+#import <JAMSVGImage/JAMStyledBezierPath.h>
+#import <JAMSVGImage/JAMSVGParser.h>
+#import <JAMSVGImage/JAMStyledBezierPathFactory.h>
+#import <JAMSVGImage/JAMSVGGradientParts.h>
+#import <JAMSVGImage/JAMSVGUtilities.h>
